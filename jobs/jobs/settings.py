865a8b55bc395869e93c12854a8eaee75acb7a53
Daniel Mandel <DMandel43@massmutual.com>
2020-08-24 16:29:58 -0400

    Created a django app dashboard
    - Created a django app dashboard
    - Added the bootstrap library
    - Created a base template to be the parent

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'jobs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            './templates/'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
