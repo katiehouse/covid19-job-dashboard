de13bec849027e4eec34724c4ef921a24e9be116
Katie House <khouse23@massmutual.com>
2020-08-25 16:46:55 -0400

    add settings

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
