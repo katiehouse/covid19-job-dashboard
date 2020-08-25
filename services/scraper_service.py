7411bcef36b6ce88591d009a648de959c2bbbf69
Daniel Mandel <DMandel43@massmutual.com>
2020-08-25 19:18:02 -0400

    Intregated the scraper and created the form
    - Integrated the scraper
    - Hooked up the form
    - Formatted the html using loops

--- services/scraper_service.py
+++ services/scraper_service.py
@@ -1,8 +1,110 @@
 # import packages
+import bs4
+from bs4 import BeautifulSoup
+import requests
 import requests
 import pandas as pd
 import time
-from functions import *
+
+# get soup object
+def get_soup(text):
+    return BeautifulSoup(text, "lxml")# , from_encoding="utf-8")
+
+
+# extract company
+def extract_company(div):
+    company = div.find_all(name="span", attrs={"class": "company"})
+    if len(company) > 0:
+        for b in company:
+            return b.text.strip()
+    else:
+        sec_try = div.find_all(name="span", attrs={"class": "result-link-source"})
+        for span in sec_try:
+            return span.text.strip()
+    return "NOT_FOUND"
+
+
+# extract job salary
+def extract_salary(div):
+    try:
+        return div.find("nobr").text
+    except:
+        try:
+            span = div.find_all(name="span", attrs={"class": "salaryText"})
+            return span[0].get_text()
+        except:
+            return "NOT_FOUND"
+    return "NOT_FOUND"
+
+
+# extract job location
+def extract_location(div):
+    for span in div.findAll("span", attrs={"class": "location"}):
+        if span.text != []:
+            return span.text
+        else:
+            for span in div.findAll("div", attrs={"class": "location"}):
+                return span.text
+    return "NOT_FOUND"
+
+
+# extract job title
+def extract_job_title(div):
+    for a in div.find_all(name="a", attrs={"data-tn-element": "jobTitle"}):
+        return a["title"]
+    return "NOT_FOUND"
+
+
+# extract jd summary
+def extract_summary(link):
+    page = requests.get(link)
+    soup = get_soup(page.text)
+    spans = soup.find_all(name="div", attrs={"id": "jobDescriptionText"})
+    for span in spans:
+        return span.text.strip()
+    return "NOT_FOUND"
+
+
+# extract link of job description
+def extract_link(div):
+    try:
+        return "https://www.indeed.com/viewjob?jk=" + div['data-jk']
+    except:
+        return "NOT_FOUND"
+
+
+# extract date of job when it was posted
+def extract_date(div):
+    try:
+        spans = div.findAll("span", attrs={"class": "date"})
+        for span in spans:
+            return span.text.strip()
+    except:
+        return "NOT_FOUND"
+    return "NOT_FOUND"
+
+
+# extract full job description from link
+def extract_fulltext(url):
+    try:
+        page = requests.get("http://www.indeed.com" + url)
+        soup = BeautifulSoup(page.text, "lxml", from_encoding="utf-8") # got rid of warning: ("You provided Unicode markup but also provided a value for from_encoding. Your from_encoding will be ignored.")
+        page.close()
+        spans = soup.findAll("div", attrs={"class": "jobsearch-jobDescriptionText"})
+        for span in spans:
+            return span.text.strip()
+    except:
+        return "NOT_FOUND"
+    return "NOT_FOUND"
+
+
+# commented this out so we can use regular logs
+# write logs to file
+# def write_logs(text):
+#     # print(text + '\n')
+#     f = open("log.txt", "a")
+#     f.write(text + "\n")
+#     f.close()
 
 
 def scrape_indeed(input_dict, max_results_per_city = 2000):
@@ -147,49 +249,51 @@ def scrape_indeed(input_dict, max_results_per_city = 2000):
                 df.loc[num] = job_post
 
             # debug add
-            write_logs(
-                ("Completed =>")
-                + "\t"
-                + city
-                + "\t"
-                + job_qry
-                + "\t"
-                + str(cnt)
-                + "\t"
-                + str(start)
-                + "\t"
-                + str(time.time() - startTime)
-                + "\t"
-                + ("file_" + str(file))
-            )
+            # write_logs(
+            #     ("Completed =>")
+            #     + "\t"
+            #     + city
+            #     + "\t"
+            #     + job_qry
+            #     + "\t"
+            #     + str(cnt)
+            #     + "\t"
+            #     + str(start)
+            #     + "\t"
+            #     + str(time.time() - startTime)
+            #     + "\t"
+            #     + ("file_" + str(file))
+            # )
 
         # saving df as a local csv file
-        df.to_json("indeed.json", orient='records')
-
+        # will save to the database instead of json
+        # df.to_json("indeed.json", orient='records')
         # df.to_csv("jobs_" + str(file) + ".csv", encoding="utf-8")
+        return df.to_json(orient='records')
 
     else:
 
         # debug add
-        write_logs(
-            ("Skipped =>")
-            + "\t"
-            + city
-            + "\t"
-            + job_qry
-            + "\t"
-            + str(-1)
-            + "\t"
-            + str(-1)
-            + "\t"
-            + str(time.time() - startTime)
-            + "\t"
-            + ("file_" + str(file))
-        )
+        # write_logs(
+        #     ("Skipped =>")
+        #     + "\t"
+        #     + city
+        #     + "\t"
+        #     + job_qry
+        #     + "\t"
+        #     + str(-1)
+        #     + "\t"
+        #     + str(-1)
+        #     + "\t"
+        #     + str(time.time() - startTime)
+        #     + "\t"
+        #     + ("file_" + str(file))
+        # )
 
         # increment file
         file = file + 1
 
-if __name__ == "__main__":
-    input = {'zipcode': 'Massachusetts', 'query': 'data'}
-    scrape_indeed(input, max_results_per_city = 20)
+# commented this out and moved everything to one generic scraper_service
+# if __name__ == "__main__":
+#     input = {'zipcode': 'Massachusetts', 'query': 'data'}
+#     scrape_indeed(input, max_results_per_city = 20)
