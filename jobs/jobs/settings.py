71b4a4b905803a81af1c461c7e188b46b774418d
Katie House <khouse23@massmutual.com>
2020-08-25 16:32:42 -0400

    configure deployment

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -26,7 +30,7 @@ SECRET_KEY = os.getenv('DDFG_JOB_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
