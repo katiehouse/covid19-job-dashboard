77ed214a78b78320af9d8fc0005615583cc10f88
Katie House <khouse23@massmutual.com>
2020-08-25 16:57:10 -0400

    update staticfiles

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -124,6 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '/jobs/staticfiles'))
+
+STATICFILES_DIRS = (
+    os.path.normpath(os.path.join(BASE_DIR, 'jobs/staticfiles')),
+)
+STATIC_URL = 'jobs/staticfiles/'
 
 django_heroku.settings(locals())
