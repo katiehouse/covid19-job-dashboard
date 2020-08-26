815484dfd9e2af73d6e0ec64befed3743a97c7d2
elivingston44 <elivingston44@massmutual.com>
2020-08-26 17:15:49 -0400

    output dictionary, functionalize

--- text_analysis/filter_skills.py
+++ text_analysis/filter_skills.py
@@ -1,27 +1,53 @@
-########################################################
-# try filtering by linkedin skills
-########################################################
-import csv
-import pandas as pd
+from csv import reader
+from json import load
+from pandas import json_normalize
 
-with open('text_analysis/linked_in_skills.csv') as f:
-    reader = csv.reader(f)
-    skills = list(reader)
+def extract_skills():
+    # read in skills corpus
+    with open('text_analysis/linked_in_skills.csv') as f:
+        reader = csv.reader(f)
+        skills = list(reader)
 
-skills = [''.join(skill) for skill in skills] 
+    # convert skills to list of strings
+    skills = [''.join(skill) for skill in skills] 
 
-with open('text_analysis/indeed.json') as f:
-        indeed = json.load(f)
-indeed_df = pd.json_normalize(indeed)
-full_text = indeed_df['full_text']
+    # read in .json
+    with open('text_analysis/indeed.json') as f:
+            indeed = json.load(f)
+    indeed_df = json_normalize(indeed)
 
-all_rows = []
-for i in range(len(full_text)):
-    row_temp = []
+    # select only necessary column
+    full_text = indeed_df['full_text']
+
+    # combine into 1 string
+    full_text2 = []
+    for row in full_text:
+        full_text2.append(row)
+    full_text = ' '.join(full_text2)
+
+    # data cleaning
+    full_text = full_text.replace('\n', ' ').lower()
+    full_text = full_text.replace("@#$%^[]{}\|`~-=", "")
+    full_text = full_text.replace(",_/:;!?<>", " ")
+
+    # create empty skills dictionary
+    # if the skill exists in the job descriptions,
+    # increment the frequency count in the dictionary
+    skill_dict = {}
     for word in skills:
-        if ' ' + word.lower() + ' ' in full_text.iloc[i].lower():
-            row_temp.append(word)
-        if ' ' + word.lower() + ',' in full_text.iloc[i].lower():
-            row_temp.append(word)
-    all_rows.append(row_temp)
+        if ' ' + word.lower() + ' ' in full_text:
+            if word in skill_dict:
+                skill_dict[word] += 1
+            else:
+                skill_dict[word] = 1
+
+    return(skill_dict)
+
+def main():
+    skill_dict = extract_skills()
+
+
+if __name__ == "__main__":
+    main()
+
 
