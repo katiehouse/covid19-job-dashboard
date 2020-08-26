dc041e07d6d298bd6a99260ff389a12c786b8d24
Hieu Nguyen <hieunv97@gmail.com>
2020-08-26 14:35:53 -0400

    Django Models for database are finished

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from .models import Zip
+from .models import Company
+from .models import Client
+from .models import Skill
+from .models import Query
 from .models import Job
+from .models import Query_Job
+from .models import Client_Skill
+from .models import Job_Skill
 
+
+admin.site.register(Zip)
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Skill)
+admin.site.register(Query)
 admin.site.register(Job)
+admin.site.register(Query_Job)
+admin.site.register(Client_Skill)
+admin.site.register(Job_Skill)
