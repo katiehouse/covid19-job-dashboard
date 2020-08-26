97cf7cca9e2dfff795d2dba8bd2a4667062d9632
erinaf <40175687+erinaf@users.noreply.github.com>
2020-08-26 19:44:00 -0400

    updating skill_dict
    Update skill_dict counter; also include 1 and 2-grams

--- text_analysis/extract_skills.py
+++ text_analysis/extract_skills.py
@@ -1,6 +1,9 @@
 from csv import reader
 from json import load
 from pandas import json_normalize
+from collections import Counter 
+from nltk import ngrams
+from nltk import word_tokenize
 
 def extract_skills():
     # read in skills corpus
@@ -33,21 +36,44 @@ def extract_skills():
     # create empty skills dictionary
     # if the skill exists in the job descriptions,
     # increment the frequency count in the dictionary
+# older code
+#     skill_dict = {}
+#     for word in skills:
+#         if ' ' + word.lower() + ' ' in full_text:
+#             if word in skill_dict:
+#                 skill_dict[word] += 1
+#             else:
+#                 skill_dict[word] = 1
+# older code end
+
+    skills = [x.lower() for x in skills]
     skill_dict = {}
-    for word in skills:
-        if ' ' + word.lower() + ' ' in full_text:
-            if word in skill_dict:
-                skill_dict[word] += 1
-            else:
-                skill_dict[word] = 1
+    for word in word_tokenize(full_text):
+        word = word.lower()
+        if word in skill_dict:
+            skill_dict[word] += 1
+        else:
+            skill_dict[word] = 1
+
+    for word in ngrams(full_text.split(), 2):
+        word = word[0]+' '+word[1].lower()
+        if word in skill_dict:
+            skill_dict[word] += 1
+        else:
+            skill_dict[word] = 1
+    
+    
+    skill_dict = dict((key,value) for key, value in skill_dict.items() if key in skills)
 
     return(skill_dict)
 
 def main():
     skill_dict = extract_skills()
+    sort_skills = sorted(skill_dict.items(), key=lambda x: x[1], reverse=True)
+    counts1 = Counter(skill_dict) 
+    high = counts1.most_common(10)
+    print(high)
 
 
 if __name__ == "__main__":
     main()
-
-
