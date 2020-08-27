a3f0ef99c1bf038bba542c9da7b0bc451a863f37
Katie House <khouse23@massmutual.com>
2020-08-27 13:57:52 -0400

    add spinner

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,12 +8,16 @@ def jobs(request):
     if request.method == 'POST':
         zipcode = request.POST.get('zipcode')
         query = request.POST.get('query')
+        max_results = 20
+        input = {'zipcode': zipcode, 'query': query}
+        jobs = json.loads(scrape_indeed(input, max_results))
+        
     else:
-        host = request.get_host()
-        zipcode = get_city(host)
-        query = 'Data Science'
-    max_results = 20
-    input = {'zipcode': zipcode, 'query': query}
-    jobs = json.loads(scrape_indeed(input, max_results))
+        #host = request.get_host()
+        #zipcode = get_city(host)
+        zipcode = ""
+        query = ""
+        input = {'zipcode': zipcode, 'query': query}
+        jobs = []
     context = {'jobs': jobs, 'input': input}
     return render(request, '../templates/jobs.html', context)
