8278847aaf69cf89c070efa2ffa5ebd89aafe669
Katie House <khouse23@massmutual.com>
2020-08-28 08:38:41 -0400

    make the dashboard faster and add some styling

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,30 +3,31 @@ from services.scraper_service import *
 import json
 from services.location_service import *
 from text_analysis.extract_skills import get_top_ten_skills
+import time
 
 
 def jobs(request):
     if request.method == 'POST':
+        startTime = time.time()
         zipcode = request.POST.get('zipcode')
         query = request.POST.get('query')
-        max_results = 20
+        max_results = 10
         input = {'zipcode': zipcode, 'query': query}
         jobs = json.loads(scrape_indeed(input, max_results))
+        print("Time to get job results: ", time.time() - startTime)
         skills = get_top_ten_skills(jobs)
-        show = False
+        print("Time to get skills: ", time.time() - startTime)
+        number_of_jobs = len(jobs)
     else:
         #host = request.get_host()
         #zipcode = get_city(host)
         zipcode = ""
         query = ""
         input = {'zipcode': zipcode, 'query': query}
-        jobs = None
+        jobs = []
         skills = []
-        show = True
-
-    if jobs == None:
         number_of_jobs = 0
-    else:
-        number_of_jobs = len(jobs)
-    context = {'jobs': jobs, 'input': input, 'skills': skills, 'show': show, 'number_of_jobs': number_of_jobs}
+
+    context = {'jobs': jobs, 'input': input, 'skills': skills,
+               'number_of_jobs': number_of_jobs}
     return render(request, '../templates/jobs.html', context)
--- text_analysis/extract_skills.py
+++ text_analysis/extract_skills.py
@@ -1,3 +1,4 @@
+import time
 import csv
 import json
 from nltk import ngrams, word_tokenize
@@ -42,6 +43,8 @@ def extract_skills(jobs):
     full_text = full_text.replace('\n', ' ').lower()
     full_text = full_text.replace('&', 'and')
     
+    
+    start_time = time.time()
     replacements1 = ['!', '@','$','%','^','[',']','{','}','\\','|','`','~','-','=', '(', ')', '.']
     for replace1 in replacements1:
         full_text = full_text.replace(replace1, '')
@@ -49,23 +52,23 @@ def extract_skills(jobs):
     replacements1 = ['!', ',','_','/',':',';','?','<','>']
     for replace1 in replacements1:
         full_text = full_text.replace(replace1, ' ')
-
     # create empty skills dictionary
     # if the skill exists in the job descriptions,
     # increment the frequency count in the dictionary
-# older code
-#     skill_dict = {}
-#     for word in skills:
-#         if ' ' + word.lower() + ' ' in full_text:
-#             if word in skill_dict:
-#                 skill_dict[word] += 1
-#             else:
-#                 skill_dict[word] = 1
-# older code end
+    # older code
+    #     skill_dict = {}
+    #     for word in skills:
+    #         if ' ' + word.lower() + ' ' in full_text:
+    #             if word in skill_dict:
+    #                 skill_dict[word] += 1
+    #             else:
+    #                 skill_dict[word] = 1
+    # older code end
 
     skills_raw = skills
 
     # data cleaning for skills
+    start_time = time.time()
     skills = [x.lower() for x in skills]
     for i in range(len(skills)):
         skills[i] = skills[i].replace('&', 'and')
@@ -93,9 +96,10 @@ def extract_skills(jobs):
     df['skills'] = skills
 
     df.to_csv('text_analysis/df_test.csv')
+    start_time = time.time()
 
     skill_dict = {}
-    for i in range(1,6):
+    for i in range(1,3):
         for word in ngrams(full_text.split(), i):
             if i == 1:
                 word = ''.join(word).lower()
@@ -106,9 +110,11 @@ def extract_skills(jobs):
             else:
                 skill_dict[word] = 1
     
-    
+    start_time = time.time()
     skill_dict = dict((key,value) for key, value in skill_dict.items() if key in skills)
-    
+    print("lines 119", time.time() - start_time)
+
+    start_time = time.time()
     temp_list = []
     for key, value in skill_dict.items():
         skill_raw = df[df['skills'] == key].iloc[0, 0]
@@ -121,7 +127,7 @@ def extract_skills(jobs):
 def get_top_ten_skills(jobs):
     skill_dict = extract_skills(jobs)
     sort_skills = sorted(skill_dict, key = lambda k:k['value'], reverse = True)
-    top_10 = sort_skills[:20]
+    top_10 = sort_skills[:30]
     print(top_10)
     return top_10
 
